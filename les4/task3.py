fa42338277e91d00119d73cfeebf54e3726f554a
Egor <yegor.rabota.gu@mail.ru>
2021-04-03 17:51:49 +0300

    les4

---
+++ les4/task3.py
@@ -0,0 +1,46 @@
+"""
+Задание 3.
+Приведен код, формирующий из введенного числа
+обратное по порядку входящих в него
+цифр и вывести на экран.
+Сделайте профилировку каждого алгоритма через cProfile и через timeit
+Сделайте вывод, какая из трех реализаций эффективнее и почему!!!
+И можете предложить еще свой вариант решения!
+Без аналитики задание считается не принятым
+"""
+
+
+def revers_1(enter_num, revers_num=0):
+    if enter_num == 0:
+        return revers_num
+    else:
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+        return revers_1(enter_num, revers_num)
+
+
+def revers_2(enter_num, revers_num=0):
+    while enter_num != 0:
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+    return revers_num
+
+
+# Это быстрее всех
+def revers_3(enter_num):
+    enter_num = str(enter_num)
+    revers_num = enter_num[::-1]
+    return revers_num
+
+
+if __name__ == '__main__':
+    import cProfile
+    import timeit
+    cProfile.run('revers_1(123)')
+    cProfile.run('revers_2(123)')
+    cProfile.run('revers_3(123)')
+    print(timeit.timeit("revers_1(123)", setup="from __main__ import revers_1"))
+    print(timeit.timeit("revers_2(123)", setup="from __main__ import revers_2"))
+    print(timeit.timeit("revers_3(123)", setup="from __main__ import revers_3"))
